95a62a3bd5024f6aa41519fb23a568f6dc061072
vishesh.m <vishesh.m@darwinbox.io>
2023-07-06 17:18:33 +0530

    final

--- bdd.sh
+++ bdd.sh
@@ -9,7 +9,21 @@ if [ -z "$CHANGED_FILES" ]; then
   exit 0
 fi
 
+# Set the coverage threshold
+THRESHOLD=50
+
 # Run code coverage for the changed files only and generate the XML report
-./node_modules/.bin/istanbul cover  ./node_modules/.bin/mocha -- -R mocha-junit-reporter
+./node_modules/.bin/nyc --reporter=cobertura --include="${CHANGED_FILES}" ./node_modules/.bin/mocha
 
+# Check the coverage threshold
+ACTUAL_COVERAGE=$(./node_modules/.bin/nyc report --reporter=text-summary | awk '{print $4}' | sed 's/%//')
+COVERAGE_EXIT_STATUS=$?
 
+# If the coverage check fails or the coverage is below the threshold, abort the build
+if [ $COVERAGE_EXIT_STATUS -ne 0 ] || (( $(echo "$ACTUAL_COVERAGE < $THRESHOLD" | bc -l) )); then
+  echo "Code coverage threshold of $THRESHOLD% not met. Aborting the build."
+  exit 1
+fi
+
+# Move the generated coverage report to the root directory with the desired name
+mv coverage/cobertura-coverage.xml ./code-coverage.xml
