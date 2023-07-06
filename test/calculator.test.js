2ee3f68a5a532075c8cf6a82124b3631baff3c8d
vishesh.m <vishesh.m@darwinbox.io>
2023-07-06 14:36:55 +0530

    first commit

---
+++ test/calculator.test.js
@@ -0,0 +1,8 @@
+const { add } = require('../src/calculator');
+const assert = require('assert');
+
+describe('Calculator', () => {
+  it('should correctly add two numbers', () => {
+    assert.strictEqual(add(2, 3), 5);
+  });
+});
