1ecb82b0bb22f97bb7f3480b618dbd0ea3aab643
vishesh.m <vishesh.m@darwinbox.io>
2023-07-06 16:33:20 +0530

    final

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,8 +1,11 @@
-const { add } = require('../src/calculator');
+const { add, sub } = require('../src/calculator');
 const assert = require('assert');
 
 describe('Calculator', () => {
   it('should correctly add two numbers', () => {
     assert.strictEqual(add(2, 3), 5);
+  });
+  it('should correctly sub two numbers', () => {
+    assert.strictEqual(sub(3,2), 1);
   });
 });
