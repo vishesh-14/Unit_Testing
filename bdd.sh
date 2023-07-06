c651f1382fc52298bc004cd7c43d4631a58a5720
vishesh.m <vishesh.m@darwinbox.io>
2023-07-06 16:03:34 +0530

    testing1

--- bdd.sh
+++ bdd.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-npm test
+
+# Run the tests with code coverage
+./node_modules/.bin/nyc --reporter=lcov --reporter=text-lcov _mocha
+
+# Calculate the coverage percentage
+./node_modules/.bin/nyc report --reporter=text-summary
+
+# Check the coverage threshold and exit with an appropriate status
+./node_modules/.bin/nyc check-coverage --lines 50
--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,8 @@
 function add(a, b) {
   return a + b;
 }
-
-module.exports = { add };
+function sub(a,b)
+{
+    return a-b;
+}
+module.exports = { add,sub};
