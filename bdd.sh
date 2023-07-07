0e3b48106def7f6c7ea0b6bd8077e7f4bd03333f
vishesh.m <vishesh.m@darwinbox.io>
2023-07-07 09:37:15 +0530

    testing

--- bdd.sh
+++ bdd.sh
@@ -10,7 +10,14 @@ if [ -z "$CHANGED_FILES" ]; then
 fi
 
 # Run code coverage for the changed files only and generate the XML report
-istanbul cover _mocha -- -R mocha-junit-reporter
+./node_modules/.bin/istanbul cover --include-all-sources --dir coverage --report cobertura --print none ./node_modules/.bin/_mocha -- -R mocha-junit-reporter
 
-# Move the generated XML report to the root directory
-mv coverage/coverage.xml ./code-coverage.xml
+# Calculate the coverage percentage for the changed files
+COVERAGE_THRESHOLD=50
+COVERAGE_RESULT=$(./node_modules/.bin/istanbul report --root coverage --include=$CHANGED_FILES lcov | grep -oP 'Lines[^%]*\K[0-9.]+')
+
+# Check if coverage meets the threshold
+if (( $(echo "$COVERAGE_RESULT < $COVERAGE_THRESHOLD" | bc -l) )); then
+  echo "Code coverage threshold of $COVERAGE_THRESHOLD% not met for changed files. Failing the build."
+  exit 1
+fi
