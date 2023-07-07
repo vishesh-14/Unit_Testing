22e08ea1e21f0b90e35d5af8c6085da24af36c8b
vishesh.m <vishesh.m@darwinbox.io>
2023-07-07 14:50:42 +0530

    final plz

--- test/temp.test.js
+++ test/temp.test.js
@@ -2,6 +2,8 @@ const { addbyone } = require('../src/test');
 const assert = require('assert');
 
 describe('Calculator', () => {
- 
+  it('should correctly add by one', () => {
+    assert.strictEqual(addbyone(2), 3);
+  });
   
 });
